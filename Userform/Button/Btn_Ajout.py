170afc5899712fb43ed95d1f9bccc284d507c527
MAE009 <emmanuelmayala40@gmail.com>
2025-09-11 21:13:34 +0100

    Update Btn_Ajout.py

--- Userform/Button/Btn_Ajout.py
+++ Userform/Button/Btn_Ajout.py
@@ -1,29 +1,27 @@
-# Ajouter un rapport 
-Private Sub Btn_Ajouter_Click()
-    Dim ws As Worksheet
-    Dim tbl As ListObject
-    Dim newRow As ListRow
-    
+Private Sub BtnAjouter_Click()
+    Dim ws As Worksheet, tbl As ListObject
     Set ws = ThisWorkbook.Sheets("RJ")
     Set tbl = ws.ListObjects("suivi_interventio")
     
+    ' Ajout d'une nouvelle ligne
+    Dim newRow As ListRow
     Set newRow = tbl.ListRows.Add
     
-    With newRow
-        .Range(1, 1).Value = Me.TxtNomBien.Value
-        .Range(1, 2).Value = Me.TxtLocalisation.Value
-        .Range(1, 3).Value = Me.CboEtat.Value
-        .Range(1, 4).Value = Me.TxtDefaillance.Value
-        .Range(1, 5).Value = Me.TxtCause.Value
-        .Range(1, 6).Value = Me.TxtAction.Value
-        .Range(1, 7).Value = Me.TxtDateDefaillance.Value
-        .Range(1, 8).Value = Me.TxtDateActuel.Value
-        .Range(1, 9).Value = Me.TxtTemps.Value
-        .Range(1, 10).Value = Me.CboResultat.Value
-        .Range(1, 11).Value = Me.TxtIntervenant.Value
-        .Range(1, 12).Value = Me.CboObservation.Value
+    With newRow.Range
+        .Cells(1, 1).Value = Me.TxtNomBien.Value
+        .Cells(1, 2).Value = Me.TxtLocalisation.Value
+        .Cells(1, 3).Value = Me.CboEtat.Value
+        .Cells(1, 4).Value = Me.TxtDefaillance.Value
+        .Cells(1, 5).Value = Me.TxtCause.Value
+        .Cells(1, 6).Value = Me.TxtAction.Value
+        .Cells(1, 7).Value = Me.TxtDateDefaillance.Value
+        .Cells(1, 8).Value = Me.TxtDateActuel.Value
+        .Cells(1, 9).Value = Me.TxtTemps.Value
+        .Cells(1, 10).Value = Me.CboResultat.Value
+        .Cells(1, 11).Value = Me.TxtIntervenant.Value
+        .Cells(1, 12).Value = Me.CboObservation.Value
     End With
     
-    Call ChargerListBox
-    MsgBox "Rapport ajouté avec succès.", vbInformation
+    Call ChargerRapports
+    MsgBox "Rapport ajouté avec succès !", vbInformation
 End Sub
