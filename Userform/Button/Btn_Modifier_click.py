bdc0962a4c9c84ac03b38a6c636bc750c7d27762
MAE009 <emmanuelmayala40@gmail.com>
2025-09-11 19:57:00 +0100

    Create Btn_Modifier_click.py

---
+++ Userform/Button/Btn_Modifier_click.py
@@ -0,0 +1,36 @@
+Private Sub Btn_Modifier_Click()
+    Dim ws As Worksheet
+    Dim tbl As ListObject
+    Dim ligne As Range
+    Dim idx As Long
+    
+    If Me.ListBox1.ListIndex = -1 Then
+        MsgBox "Veuillez sélectionner un rapport à modifier.", vbExclamation
+        Exit Sub
+    End If
+    
+    Set ws = ThisWorkbook.Sheets("RJ")
+    Set tbl = ws.ListObjects("suivi_interventio")
+    
+    idx = Me.ListBox1.ListIndex + 1 ' Décalage car ListBox commence à 0
+    
+    Set ligne = tbl.DataBodyRange.Rows(idx)
+    
+    With ligne
+        .Cells(1, 1).Value = Me.TxtNomBien.Value
+        .Cells(1, 2).Value = Me.TxtLocalisation.Value
+        .Cells(1, 3).Value = Me.CboEtat.Value
+        .Cells(1, 4).Value = Me.TxtDefaillance.Value
+        .Cells(1, 5).Value = Me.TxtCause.Value
+        .Cells(1, 6).Value = Me.TxtAction.Value
+        .Cells(1, 7).Value = Me.TxtDateDefaillance.Value
+        .Cells(1, 8).Value = Me.TxtDateActuel.Value
+        .Cells(1, 9).Value = Me.TxtTemps.Value
+        .Cells(1, 10).Value = Me.CboResultat.Value
+        .Cells(1, 11).Value = Me.TxtIntervenant.Value
+        .Cells(1, 12).Value = Me.CboObservation.Value
+    End With
+    
+    Call ChargerListBox
+    MsgBox "Rapport modifié avec succès.", vbInformation
+End Sub
